2019e8b52e053098419d79916a5dc497133160c4
kramonex <kramonex1@yandex.ru>
2023-04-02 14:08:28 +0300

    settings main view

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var userName: UITextField!
+    @IBOutlet var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
+    @IBAction func logInButtonTapped(_ sender: UIButton) {
+        if userName.text == "User" && password.text == "12345" {
+            
+        } else {
+            showAlert(withTitle: "Invalid login or password ",
+                      andMessage: "Please, enter correct login and password")
+        }
+        
+    }
+    
+    @IBAction func forgotUserNameButtonTapped(_ sender: UIButton) {
+        showAlert(withTitle: "Ooops!", andMessage: "Your name is User")
+    }
+    
+    @IBAction func forgotPasswordButtonTapped(_ sender: UIButton) {
+        showAlert(withTitle: "Ooops!", andMessage: "Your password is 12345")
+    }
+    
+    
+    private func showAlert(withTitle title: String, andMessage message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.password.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
 }
 
