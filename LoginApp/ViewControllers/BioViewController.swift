f4711b766b613638eb123ce632bcd89ab5cb2a6b
kramonex <kramonex1@yandex.ru>
2023-04-04 22:23:59 +0300

    still settings

---
+++ LoginApp/ViewControllers/BioViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BioViewController.swift
+//  LoginApp
+//
+//  Created by kramonex on 04.04.2023.
+//
+
+import UIKit
+
+class BioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+
+}
---
+++ LoginApp/ViewControllers/ProfileNavigationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileNavigationViewController.swift
+//  LoginApp
+//
+//  Created by kramonex on 04.04.2023.
+//
+
+import UIKit
+
+class ProfileNavigationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
