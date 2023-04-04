dc3ebb0c48c742d465e7c9c14fbede3d01f3cf52
kramonex <kramonex1@yandex.ru>
2023-04-04 22:03:21 +0300

    work

---
+++ LoginApp/Models/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  LoginApp
+//
+//  Created by kramonex on 04.04.2023.
+//
+
+
+struct User {
+    let login: String
+    let password: String
+    let person: String
+}
+
+struct Person {
+    let name: String
+    let surname: String
+    let age: Int
+}
